71b9b8035e263e3a06f8c8afc279758f6ff170b0
Eduardo Garcia <dev.lalo.desarrollo@gmail.com>
2021-02-13 14:03:23 -0600

    Structure of project

--- lib/config/Theme.dart
+++ lib/config/Theme.dart
@@ -3,13 +3,23 @@ import 'package:flutter/material.dart';
 class SuperSimpleTheme {
   static const primary = Color(0xFF3DC135);
   static const secondary = Color(0xFF2EAFAD);
+  static const textColor = Colors.white;
+
 }
 
 ThemeData get principalTheme {
   return ThemeData(
-      primaryColor: SuperSimpleTheme.primary,
-      accentColor: SuperSimpleTheme.secondary,
-      backgroundColor: SuperSimpleTheme.primary,
-      scaffoldBackgroundColor: SuperSimpleTheme.primary,
-      appBarTheme: AppBarTheme(color: SuperSimpleTheme.primary));
+    primaryColor: SuperSimpleTheme.primary,
+    accentColor: SuperSimpleTheme.secondary,
+    backgroundColor: SuperSimpleTheme.primary,
+    scaffoldBackgroundColor: SuperSimpleTheme.primary,
+    appBarTheme: AppBarTheme(color: SuperSimpleTheme.primary),
+    textTheme: TextTheme(
+      bodyText1: TextStyle(),
+      bodyText2: TextStyle(),
+    ).apply(
+      bodyColor: SuperSimpleTheme.textColor,
+      
+    ),
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toka/config/Routes.dart';
+import 'package:toka/config/Theme.dart';
+import 'package:toka/provider/LoginProvider.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,12 +10,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [],
+      providers: [
+        Provider<LoginProvider>(create: (_)=>LoginProvider(),)
+      ],
       child: MaterialApp(
         title: 'Toka Users',
         debugShowCheckedModeBanner: false,
         onGenerateRoute: routes,
         initialRoute: "login",
+        theme: principalTheme,
       ),
     );
   }
